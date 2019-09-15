b3e9dcbda5253d1749e3b355be03f92c7f359bbf
Kizashi Nagata <kizashi1122@gmail.com>
2019-09-15 23:38:34 +0900

    fix warning

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,14 +2,13 @@ require 'simplecov'
 require 'coveralls'
 Coveralls.wear!
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::HTMLFormatter,
   Coveralls::SimpleCov::Formatter
-]
+])
 SimpleCov.start do
   add_filter '.bundle/'
 end
 
 require 'rubygems'
 require 'mail_address'
-
